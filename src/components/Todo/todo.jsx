4aa57a22a868893343bf9689da360a13ea74ebf0
zdeep10 <gabriel.wti02@gmail.com>
2022-03-07 22:28:24 -0300

    Building Todo list

--- src/components/Todo/todo.jsx
+++ src/components/Todo/todo.jsx
@@ -1,7 +1,59 @@
 import "./todo.scss";
+import trash from "../../icons/delete-icon.png";
+import add from "../../icons/add-icon.png";
 
 function Todo() {
-  return <h1>Testando</h1>;
+  return (
+    <div className="todo__container">
+      <div className="todo__wrraper">
+        <header className="todo__header">
+          <h1 className="todo__header__title">Welcome to your Todo List</h1>
+          <h2>Your tasks</h2>
+        </header>
+
+        <main className="todo__main">
+          <div className="teste">
+            <div className="todo__main__filds">
+              <input
+                id="01"
+                className="todo__main__checkbox"
+                type="checkbox"
+                name="r"
+                value="1"
+              />
+              <label for="01">Levar lixo para fora</label>
+            </div>
+
+            <div className="todo__main__delete">
+              <img src={trash} alt="" />
+            </div>
+          </div>
+          <div className="teste">
+            <div className="todo__main__filds">
+              <input
+                id="01"
+                className="todo__main__checkbox"
+                type="checkbox"
+                name="r"
+                value="1"
+              />
+              <label for="01">Levar lixo para fora</label>
+            </div>
+
+            <div className="todo__main__delete">
+              <img src={trash} alt="" />
+            </div>
+          </div>
+        </main>
+
+        <footer className="todo__footer">
+          <button className="todo__footer__new">
+            <img src={add} alt="" />
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default Todo;
