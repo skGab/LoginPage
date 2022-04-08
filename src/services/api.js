faf17fa14aca945bdd4662e5db6ddbb5e4eea647
zdeep10 <gabriel.wti02@gmail.com>
2022-04-08 00:23:21 -0300

    customizando ScrollBar

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://nodeaplication.herokuapp.com",
-  baseURL: "http://localhost:3001",
+  // baseURL: "http://localhost:3001",
 });
 
 export default api;
